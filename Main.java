c27631990312ca64140ac92d48575583908dd1b1
ttdangg <ttdang1445@gmail.com>
2024-11-08 09:54:51 -0800

    isPrime complete

--- Main.java
+++ Main.java
@@ -5,13 +5,24 @@ public class Main
   public static void main(String[] args)
   {
     // You can test your method here
-    // System.out.println(isPrime(1));
-    // System.out.println(isPrime(17));
+    System.out.println(isPrime(1));
+    System.out.println(isPrime(17));
+    System.out.println(isPrime(4));
+
   }
 
   public static boolean isPrime(int N)
   {
-    // TODO: Replace and write your code below
-    return false;
+    if(N==1){
+      return false;
+    }
+    for(int i = 2; i<N; i++){
+      if (N%i == 0){
+        return false;
+      }
+
+    }
+
+    return true;
   }
 }
